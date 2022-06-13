b53b3702b9842434415225483c0d9baf9c4c6ccc
Thomasprss <thomas.piersson@gmail.com>
2022-06-13 11:06:40 +0200

    adding wireshark installation

--- attack/scripts/install_attack.sh
+++ attack/scripts/install_attack.sh
@@ -7,6 +7,7 @@ APT_OPT="-o Dpkg::Progress-Fancy="0" -q -y"
 
 echo "START - debian configuration as a kali - "$IP
 
+echo "[1] install nmap, wfuzz, ettercap..."
 apt-get install $APT_OPT \
 	curl wget gnupg2 \
 	software-properties-common \
@@ -14,12 +15,12 @@ apt-get install $APT_OPT \
 	ettercap-common \
 	wfuzz \
 
-
-echo "[2] install metaqploit... "
+echo "[2] install metasploit... "
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
 chmod +x msfinstall
 ./msfinstall
 
-
-
+echo "[3] install wireshark... "
+echo "wireshark-common wireshark-common/install-setuid boolean true" | debconf-set-selections
+DEBIAN_FRONTEND=noninteractive apt-get -y install wireshark >> /dev/null
 
